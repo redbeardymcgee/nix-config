a47c3f9c84dc8ea37c55b018d7d4444659b3f180
redbeardymcgee <redbeardymcgee@gmail.com>
2024-06-26 19:21:45 -0500

    greeter fixed. numlock not.

--- hosts/common/services/greetd.nix
+++ hosts/common/services/greetd.nix
@@ -4,7 +4,7 @@
       settings = {
         default_session = {
           command = /*sh*/ ''
-          ${pkgs.numlockx}/bin/numlockx on
+          ${pkgs.numlockx}/bin/numlockx on &
           ${pkgs.greetd.tuigreet}/bin/tuigreet \
             --asterisks \
             --cmd ${pkgs.river}/bin/river \
@@ -12,7 +12,8 @@
             --remember-user-session \
             --theme "border=magenta;text=cyan;prompt=green;time=red;action=blue;button=yellow;container=black;input=red" \
             --time \
-            --user-menu
+            --user-menu \
+            --debug
           '';
           user = "greeter";
         };
