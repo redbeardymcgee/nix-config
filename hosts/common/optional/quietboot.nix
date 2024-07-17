2ba8c0effa7950c90797ba180c2021ff660ceefa
redbeardymcgee <redbeardymcgee@gmail.com>
2024-07-17 18:47:43 -0500

    boot SHOULD be fixed

--- hosts/common/optional/quietboot.nix
+++ hosts/common/optional/quietboot.nix
@@ -1,59 +1,32 @@
 {pkgs, ...}: {
   console = {
     earlySetup = true;
-    font = "ter-u12n";
-    packages = with pkgs; [terminus_font];
+    font = "${pkgs.terminus_font}/share/consolefonts/ter-v32n.psf.gz";
     useXkbConfig = true;
   };
 
   boot = {
-    # plymouth = {
-    #   enable = false;
-    #
-    #   theme = "pixels";
-    #   themePackages = with pkgs; [
-    #     (adi1090x-plymouth-themes.override {
-    #       selected_themes = ["pixels"];
-    #     })
-    #   ];
-    # };
-
-    #loader.timeout = 0;
+    consoleLogLevel = 0;
+    loader.timeout = 0;
 
     kernelParams = [
       "boot.shell_on_fail"
       "loglevel=3"
-      # "quiet"
-      # "rd.systemd.show_status=false"
+      "quiet"
+      "rd.systemd.show_status=false"
       "rd.udev.log_level=3"
-      # "splash"
+      "splash"
     ];
 
-    consoleLogLevel = 0;
+    plymouth = {
+      enable = false;
 
-    #initrd = {
-      # systemd = {
-      #   enable = true;
-      #
-      #   units = {
-      #     activate-numlock = {
-      #       name = "activate-numlock.service";
-      #       text = ''
-      #         [Unit]
-      #         Description=Activate numlock before login
-      #
-      #         [Service]
-      #         ExecStart=${pkgs.kbd}/bin/setleds +num
-      #
-      #         [Install]
-      #         WantedBy=multi-user.target
-      #       '';
-      #     };
-      #   };
-      # };
-
-      # kernelModules = ["nvidia"];
-      #verbose = false;
-    #};
+      theme = "pixels";
+      themePackages = with pkgs; [
+        (adi1090x-plymouth-themes.override {
+          selected_themes = ["pixels"];
+        })
+      ];
+    };
   };
 }
