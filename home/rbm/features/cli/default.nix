146591d655aa8f876a831c3211c063687a97fb91
redbeardymcgee <redbeardymcgee@gmail.com>
2024-07-17 18:25:02 -0500

    switch over to new cli updates

--- home/rbm/features/cli/default.nix
+++ home/rbm/features/cli/default.nix
@@ -11,25 +11,19 @@
     ./bottom.nix
     ./direnv.nix
     ./eza.nix
-    ./fastfetch.nix
-    # TODO: Remove this after https://gitlab.com/Zaney/zaneyos/-/merge_requests/52
-    #       decision.
-    #
-    #       This is only here as a shortcut to test their config
-    #       without having to build a ZaneyOS VM. I'm not sure
-    #       if I even care to keep a fastfetch config of my own.
-    # ./fastfetch
+    ./fastfetch
     ./fd.nix
     ./fish.nix
     ./fzf.nix
     ./git.nix
-    ./gitui.nix
     ./go.nix
     ./htop.nix
     ./jq.nix
+    ./lazygit.nix
     ./less.nix
     ./man.nix
     ./nix-index.nix
+    ./nvim.nix
     # ./rbw.nix
     ./ripgrep.nix
     ./ssh.nix
@@ -40,15 +34,6 @@
     ./zk.nix
     ./zoxide.nix
   ];
-
-  nixpkgs = {
-    overlays = builtins.attrValues outputs.overlays ++ builtins.attrValues inputs.kixvim.overlays;
-
-    config = {
-      allowUnfree = true;
-      allowUnfreePredicate = _: true;
-    };
-  };
 
   home.packages = with pkgs; [
     alejandra
@@ -63,7 +48,6 @@
     lazydocker
     manix
     miller
-    neovim
     # kixvim
     nodejs
     ouch
@@ -71,6 +55,7 @@
     procs
     python3
     ripdrag
+    scc
     transmission_4
     tree-sitter
     yq
