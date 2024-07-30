4a00bd0c23ad12e1a77c75c93b1fd9184828ed70
redbeardymcgee <redbeardymcgee@gmail.com>
2024-07-30 11:35:57 -0500

    updated flake inputs

--- flake.nix
+++ flake.nix
@@ -14,6 +14,10 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.05";
+    lix = {
+      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.90.0.tar.gz";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     firefox-addons = {
       url = "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -27,10 +31,6 @@
     # nur.url = "github:nix-community/NUR";
     stylix.url = "github:danth/stylix";
     yazi.url = "github:sxyazi/yazi";
-    lix = {
-      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.90.0.tar.gz";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
