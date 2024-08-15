8b12ef97c9d21c141f48e8fc7be30be07ec92d05
redbeardymcgee <redbeardymcgee@gmail.com>
2024-08-15 14:37:39 -0500

    update window rules & misc

--- home/rbm/features/desktop/river/river.nix
+++ home/rbm/features/desktop/river/river.nix
@@ -4,8 +4,19 @@
   ...
 }: {
   home.packages = [pkgs.rivercarro];
+  # xdg.desktopEntries."river" = {
+  #   name = "River";
+  #   comment = "A dynamic tiling Wayland compositor";
+  #   exec = "river";
+  #   type = "Application";
+  # };
+
   wayland.windowManager.river = {
     enable = true;
+    systemd = {
+      enable = true;
+      variables = ["-all"];
+    };
 
     settings = let
       pow2 = n:
@@ -177,59 +188,74 @@
 
       rule-add = {
         "-app-id" = {
-          # Send to Samsung 1440p
-          "*_ultrawide" = "output DP-1";
-          "vesktop" = "output DP-1";
-
-          # Manage Steam windows carefully
-          "steam" = "float";
-          # Dump main steam app on tag 7
-          "*steam" = "tags ${toString (pow2 6)}";
-          # Put games on tag 6
-          "steam_app*" = "tags ${toString (pow2 5)}";
-          # Trimps
-          "steam_app_1877960" = "output DP-1";
-          # "SDL Application" = "tags ${toString (pow2 5)}";
-
-          # Float some apps
           "xdg-desktop-portal-*" = "float";
-
-          # Startup terminals
+          "Xdg-desktop-portal-*" = "float";
+          "terminal_*" = "output DVI-D-1";
           "terminal_localhost" = "tags ${toString (pow2 0)}";
           "terminal_projects" = "tags ${toString (pow2 1)}";
           "terminal_mcgeedia" = "tags ${toString (pow2 2)}";
+          "firefox_ultrawide" = "output DP-1";
+          "*firefox_ultrawide" = "tags ${toString (pow2 0)}";
+          "vesktop" = "output DP-1";
+          "*vesktop" = "tags ${toString (pow2 0)}";
 
-          # Hide this on tag 9
           "org.remmina.Remmina" = "output DP-1";
-          "org.remmina.Remmin*" = "tags ${toString (pow2 8)}";
+          "*org.remmina.Remmina" = "tags ${toString (pow2 8)}";
+
+          "*steam_app" = "tags ${toString (pow2 4)}";
+          "steam_app_*" = "output DVI-D-1";
+
+          "firefox_incr-*" = "output DP-1";
+          "firefox_incr-ngsc " = "tags ${toString (pow2 7)}"; # NGSC
+          "firefox_incr-evolve " = "tags ${toString (pow2 7)}"; # Evolve
+
+          "cs2" = "tags ${toString (pow2 4)}"; # Counter-Strike 2
+
+          "steam_app_1877960" = "output DVI-D-1"; # Trimps
+          "*steam_app_1877960" = "tags ${toString (pow2 7)}";
+
+          "steam_app_610080" = "output DP-1"; # Realm Grinder
+          "*steam_app_610080" = "tags ${toString (pow2 7)}";
+
+          "steam_app_1147690" = "output DP-1"; # NGU
+          "steam_app_1147690*" = "float";
+          "*steam_app_1147690" = "tags ${toString (pow2 6)}";
+
+          "steam_app_2373630" = "output DVI-D-1"; # Moonring
+          "*steam_app_2373630" = "tags ${toString (pow2 4)}";
         };
 
         "-title" = {
           "Picture-in-Picture" = "float";
           "Steam" = "tags ${toString (pow2 6)}";
-          "Steam*" = "float";
+          "*Steam*" = "float";
+          "Loop Hero" = "tags ${toString (pow2 4)}";
+          "*Loop Hero" = "float";
         };
       };
 
       spawn = [
-        # Layout manager
+        # NOTE: Inner quotes are required
+        #       This is mostly like `sh -c '$spawn_cmd'`
+
+        # FIXME: This shouldn't be necessary because of systemd.variables = ["-all"];
+        "'dbus-update-activation-environment --all --systemd'"
+
         # TODO: riverguile seems to have more features
         "'rivercarro -outer-gaps 0'"
 
-        # NOTE: Inner quotes are required
-        #       This is mostly like `sh -c '$spawn_cmd'`
-        " 'systemctl --user import-environment QT_QPA_PLATFORMTHEME XDG_SEAT' "
         '''way-displays > "$XDG_RUNTIME_DIR/way-displays.$XDG_SEAT.log" 2>&1' ''
         "yambar"
         "vesktop"
         "'firefox --name firefox_ultrawide'"
+        # FIXME: Firefox refuses to launch new instances of the same profile
+        # "'firefox --new-instance --name firefox_incr-ngsc https://ngsc.freddecgames.com/'"
 
         "${pkgs.writeShellScript "wayland-terminal-session-launch" ''
           for sesh in localhost mcgeedia projects
           do
-            foot --app-id=terminal_$sesh tmuxp load -y $sesh &
             # wezterm start --always-new-process --class terminal_$sesh -- tmuxp load -y $sesh &
-            sleep 2
+            foot --app-id=terminal_$sesh tmuxp load -y $sesh & sleep 1
           done
         ''}
         "
