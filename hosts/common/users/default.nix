fe01e9338a66d774b43f14f121d6a15e05d23496
redbeardymcgee <redbeardymcgee@gmail.com>
2024-10-06 18:59:49 -0500

    tidy

--- hosts/common/users/default.nix
+++ hosts/common/users/default.nix
@@ -1,21 +1,6 @@
 {
-  inputs,
-  outputs,
-  ...
-}: {
   imports = [
-    inputs.home-manager.nixosModules.home-manager
     ./rbm
   ];
-
-  home-manager = {
-    extraSpecialArgs = {
-      inherit inputs outputs;
-    };
-    useGlobalPkgs = true;
-    useUserPackages = true;
-    backupFileExtension = "backup";
-  };
-
   users.mutableUsers = false;
 }
