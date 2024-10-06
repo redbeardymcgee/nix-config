1768a3db605e9da570dff291e7f1ed7c7384ecd8
redbeardymcgee <redbeardymcgee@gmail.com>
2024-10-06 18:59:39 -0500

    update groups, tidy

--- hosts/common/users/rbm/default.nix
+++ hosts/common/users/rbm/default.nix
@@ -10,9 +10,9 @@ in {
     description = "josh";
     hashedPassword = "$6$eiZ2Cxt3CKj4cZ21$VS4lcMg21wOc1XTmf42eQK993hhqA2SVh/cAKKJCUkJCdglgrFJwU1.XL4cKVd1MQpyMe3oJoUKPIuGO2RU.l/";
     shell = pkgs.bash;
-
     extraGroups = ifExist [
       "audio"
+      "dialout"
       "docker"
       "gamemode"
       "libvirtd"
@@ -22,12 +22,7 @@ in {
       "podman"
       "video"
       "wheel"
-    ];
-
-    openssh.authorizedKeys.keys = [
-      # TODO: Add your SSH public key(s) here, if you plan on using SSH to connect
-      #
-      # openssh.authorizedKeys.keys = lib.splitString "\n" (builtins.readFile ../../../../home/rbm/ssh.pub);
+      "wireshark"
     ];
   };
 }
