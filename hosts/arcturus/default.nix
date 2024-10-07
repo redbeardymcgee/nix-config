f09849b6d86544a80e5164ebb968124a60587330
redbeardymcgee <redbeardymcgee@gmail.com>
2024-10-06 19:05:04 -0500

    enable polkit, kmscon

--- hosts/arcturus/default.nix
+++ hosts/arcturus/default.nix
@@ -8,7 +8,7 @@
 
     ../common/optional/dygma.nix
     ../common/optional/gamemode.nix
-    # ../common/optional/kmscon.nix
+    ../common/optional/kmscon.nix
     ../common/optional/podman.nix
     ../common/optional/quietboot.nix
     # ../common/optional/steam.nix
@@ -20,7 +20,9 @@
     ../common/services/keyring.nix
     ../common/services/udisks2.nix
     ../common/services/jellyfin.nix
-    # ../common/services/transmission.nix
+    ../common/services/polkit.nix
+    # ../common/services/protonmail.nix
+    # ../common/services/wormhole.nix
 
     ../common/users
   ];
@@ -33,7 +35,6 @@
       device = "/dev/disk/by-uuid/0b8b0c5d-8363-43a5-8a3a-a71fa3c7d953";
       fsType = "ext4";
       mountPoint = "/mnt/2tb";
-
       options = [
         "users"
         "exec"
