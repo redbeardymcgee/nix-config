86d51a42cb696747b013eee675390817fd528c82
redbeardymcgee <redbeardymcgee@gmail.com>
2024-12-09 18:11:00 -0600

    add sunshine

--- hosts/common/services/sunshine.nix
+++ hosts/common/services/sunshine.nix
@@ -1,12 +1,12 @@
 {
-# networking.firewall = {
-#   enable = true;
-#   allowedTCPPorts = [ 47984 47989 47990 48010 ];
-#   allowedUDPPortRanges = [
-#     { from = 47998; to = 48000; }
-#     { from = 8000; to = 8010; }
-#   ];
-# };
+  # networking.firewall = {
+  #   enable = true;
+  #   allowedTCPPorts = [ 47984 47989 47990 48010 ];
+  #   allowedUDPPortRanges = [
+  #     { from = 47998; to = 48000; }
+  #     { from = 8000; to = 8010; }
+  #   ];
+  # };
 
   services.sunshine = {
     enable = true;
@@ -14,9 +14,34 @@
     capSysAdmin = true;
     openFirewall = true;
 
-    # settings = {
-    #   sunshine_name = "arcturus";
-    #   back_button_timeout = 2000;
-    # };
+    settings = {
+      back_button_timeout = 2000;
+      # global_prep_cmd = "riverctl set-focused-tags 64";
+    };
+
+    applications = {
+      apps = [
+        {
+          name = "Desktop";
+          image = "desktop.png";
+        }
+
+        {
+          name = "Big Picture - Flatpak";
+          # cmd = "flatpak run com.valvesoftware.Steam";
+          detached = [
+            "flatpak run com.valvesoftware.Steam"
+            # "flatpak run com.valvesoftware.Steam --command='steam steam://open/bigpicture'"
+          ];
+        }
+
+        {
+          name = "Big Picture";
+          detached = [
+            "setsid steam steam://open/bigpicture"
+          ];
+        }
+      ];
+    };
   };
 }
