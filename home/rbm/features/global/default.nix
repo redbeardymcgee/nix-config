7611c49f8afc6584b61d1a9e376153badcb92cbe
redbeardymcgee <redbeardymcgee@gmail.com>
2025-02-09 12:03:18 -0600

    move stylix to module

--- home/rbm/features/global/default.nix
+++ home/rbm/features/global/default.nix
@@ -8,7 +8,9 @@
   imports =
     [
       ./nixpkgs.nix
+      ./stylix.nix
       ./xdg-user-dirs.nix
+
     ]
     ++ (builtins.attrValues outputs.homeManagerModules);
 
@@ -109,38 +111,5 @@
     };
   };
 
-  stylix = {
-    enable = true;
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/horizon-terminal-dark.yaml";
-    polarity = "dark";
-
-    cursor = {
-      package = pkgs.bibata-cursors;
-      name = "Bibata-Modern-Ice";
-      size = 24;
-    };
-
-    fonts = {
-      monospace = {
-        package = pkgs.nerdfonts.override {fonts = ["FiraCode"];};
-        name = "FiraCode Nerd Font";
-      };
-
-      sizes = {
-        applications = 13;
-        terminal = 11;
-        desktop = 14;
-        popups = 14;
-      };
-    };
-
-    image = pkgs.fetchurl {
-      url = "https://w.wallhaven.cc/full/qz/wallhaven-qzelxl.jpg";
-      hash = "sha256-WVXnEV/SHMZ0idEsLe/gdSM4gS29xsx+r9SnSnI56ts=";
-    };
-
-    opacity = {
-      terminal = 0.95;
-    };
-  };
+  
 }
