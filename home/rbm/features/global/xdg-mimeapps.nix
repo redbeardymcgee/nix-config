0df940c33c82ac665da53d881fb58f1cdbaa68e3
redbeardymcgee <redbeardymcgee@gmail.com>
2025-02-09 12:05:57 -0600

    move xdg mimeapps to module

---
+++ home/rbm/features/global/xdg-mimeapps.nix
@@ -0,0 +1,47 @@
+{
+xdg.mimeApps = {
+    enable = true;
+    defaultApplications = {
+      "application/vnd.mozilla.xul+xml" = ["org.qutebrowser.qutebrowser.desktop"];
+      "application/x-7z-compressed" = ["yazi.desktop"];
+      # "application/x-bittorrent" = ["org.qutebrowser.qutebrowser.desktop"];
+      "application/x-bzip2" = ["yazi.desktop"];
+      "application/xhtml+xml" = ["org.qutebrowser.qutebrowser.desktop"];
+      "application/x-qpwgraph-patchbay" = ["org.rncbc.qpwgraph.desktop"];
+      # "application/x-rar" = ["org.qutebrowser.qutebrowser.desktop"];
+      "application/x-shellscript" = ["redvim.desktop"];
+      "application/x-tar" = ["yazi.desktop"];
+      "application/x-xz" = ["yazi.desktop"];
+      "application/*zip" = ["yazi.desktop"];
+      # "audio/*" = ["org.qutebrowser.qutebrowser.desktop"];
+      # "image/heic" = ["org.qutebrowser.qutebrowser.desktop"];
+      "image/jpeg" = ["satty.desktop"];
+      # "image/jxl" = ["org.qutebrowser.qutebrowser.desktop"];
+      "image/png" = ["satty.desktop"];
+      # "image/svg+xml" = ["org.qutebrowser.qutebrowser.desktop"];
+      "inode/directory" = ["yazi.desktop"];
+      # "text/csv" = ["org.qutebrowser.qutebrowser.desktop"];
+      "text/english" = ["redvim.desktop"];
+      "text/html" = ["org.qutebrowser.qutebrowser.desktop"];
+      "text/plain" = ["redvim.desktop"];
+      "text/x-c++hdr" = ["redvim.desktop"];
+      "text/x-chdr" = ["redvim.desktop"];
+      "text/x-c" = ["redvim.desktop"];
+      "text/x-c++" = ["redvim.desktop"];
+      "text/x-c++src" = ["redvim.desktop"];
+      "text/x-csrc" = ["redvim.desktop"];
+      "text/x-java" = ["redvim.desktop"];
+      "text/x-makefile" = ["redvim.desktop"];
+      "text/xml" = ["org.qutebrowser.qutebrowser.desktop"];
+      "text/x-moc" = ["redvim.desktop"];
+      "text/x-pascal" = ["redvim.desktop"];
+      "text/x-tcl" = ["redvim.desktop"];
+      "text/x-tex" = ["redvim.desktop"];
+      "x-scheme-handler/http" = ["org.qutebrowser.qutebrowser.desktop"];
+      "x-scheme-handler/https" = ["org.qutebrowser.qutebrowser.desktop"];
+      # "x-scheme-handler/http" = ["firefox.desktop"];
+      # "x-scheme-handler/https" = ["firefox.desktop"];
+      "x-scheme-handler/matrix" = ["org.gnome.Fractal.desktop"];
+    };
+  };
+}
