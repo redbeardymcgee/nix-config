453dba8d48723bec2a936ad2d17fe117f8238592
redbeardymcgee <redbeardymcgee@gmail.com>
2025-02-09 11:49:08 -0600

    enable ghosty xdg config

--- home/rbm/features/desktop/ghostty.nix
+++ home/rbm/features/desktop/ghostty.nix
@@ -6,8 +6,14 @@
   home.packages = [
     inputs.ghostty.packages.x86_64-linux.default
   ];
-  xdg.configFile."ghostty/config" =
-    pkgs.writeText "ghostty-config" ''
-      font-family = FiraCode Nerd Font
-    '';
+
+  xdg.configFile."ghostty/config" = {
+    enable = true;
+
+    source = pkgs.writeText "ghostty-config" ''
+    font-family = FiraCode Nerd Font
+    window-decoration = false
+    gtk-titlebar = false
+  '';
+  };
 }
