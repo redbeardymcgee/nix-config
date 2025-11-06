0ba76a02e6f562f576426725655b010488cfdd95
redbeardymcgee <redbeardymcgee@proton.me>
2025-11-05 22:28:50 -0600

    add chat tmux workspace for signal & matrix

---
+++ home/rbm/features/cli/tmux/chat.nix
@@ -0,0 +1,47 @@
+{pkgs, ...}: {
+  xdg.configFile = {
+    "tmuxp/chat.yaml" = {
+      enable = true;
+
+      source = (pkgs.formats.yaml {}).generate "chat.yaml" {
+        session_name = "home  ";
+        windows = let
+          layout = "fd95,212x55,0,0{105x55,0,0,38,106x55,106,0,39}";
+          options = {
+            automatic-rename = false;
+            remain-on-exit = true;
+          };
+        in [
+          {
+            inherit options;
+            window_name = "monitor";
+            window_index = 0;
+            panes = [{shell = "~/.nix-profile/bin/btm";}];
+          }
+
+          {
+            inherit options;
+            window_name = "host";
+            panes = [{shell = "~/.nix-profile/bin/yazi";}];
+          }
+
+          {
+            inherit layout options;
+            window_name = "chat";
+            focus = true;
+            panes = [
+              {
+                shell = "~/.nix-profile/bin/iamb";
+                focus = true;
+              }
+              {
+                shell = "~/.nix-profile/bin/gurk";
+                focus = true;
+              }
+            ];
+          }
+        ];
+      };
+    };
+  };
+}
--- home/rbm/features/cli/tmux/default.nix
+++ home/rbm/features/cli/tmux/default.nix
@@ -2,10 +2,11 @@
   imports = [
     ./tmux.nix
 
+    ./chat.nix
     ./localhost.nix
     ./mcgeedia.nix
-    ./perseus.nix
     ./notes.nix
+    ./perseus.nix
     ./projects.nix
     ./work.nix
   ];
