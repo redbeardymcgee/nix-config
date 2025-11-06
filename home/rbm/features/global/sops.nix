b4d6c18fae99925a52f76a23cb158ffbba1beee0
redbeardymcgee <redbeardymcgee@proton.me>
2025-11-05 19:49:26 -0600

    enable sops for secrets management

---
+++ home/rbm/features/global/sops.nix
@@ -0,0 +1,10 @@
+{config, ...}: {
+  sops = {
+    defaultSopsFile = ./secrets/rbm.yaml;
+    defaultSopsFormat = "yaml";
+    age = {
+      keyFile = "${config.xdg.configHome}/sops/age/keys.txt";
+    };
+    secrets.phone_number = {};
+  };
+}
