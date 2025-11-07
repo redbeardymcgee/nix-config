8fabb9c75c4f5472d76bbf6ddd6f637ba18fd116
redbeardymcgee <redbeardymcgee@proton.me>
2025-11-06 21:14:51 -0600

    remove extraneous nvim
    use nixCats instead

--- home/rbm/features/cli/nvim/lua/config/options.lua
+++
@@ -1,9 +0,0 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
-
-vim.g.neovide_transparency = 0.80
-
-vim.g.copilot_proxy = "127.0.0.1:11435"
--- vim.g.copilot_proxy_port = 11435
-vim.g.copilot_proxy_strict_ssl = false
