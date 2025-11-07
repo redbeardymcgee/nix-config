0637c56b4c8ff88fe597a5df4bccfd6b28dac09d
redbeardymcgee <redbeardymcgee@proton.me>
2025-11-06 21:16:17 -0600

    add gurk signal messenger

--- home/rbm/features/cli/gurk.nix
+++ home/rbm/features/cli/gurk.nix
@@ -1,11 +1,36 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  config,
+  ...
+}: {
   home.packages = [pkgs.gurk-rs];
 
-  xdg.configFile."gurk/gurk.toml" = {
+  xdg.configFile."gurk/gurk.toml" = let
+    cfg = config.sops.templates.gurk.path;
+    cfgSource = config.lib.file.mkOutOfStoreSymlink cfg;
+  in {
     enable = true;
 
-    source = (pkgs.formats.toml {}).generate "gurk.toml" {
-      default_bindings = true;
+    source = cfgSource;
+  };
+
+  sops.templates."gurk" = {
+    file = (pkgs.formats.toml {}).generate "gurk.toml" {
+      default_keybindings = true;
+
+      user = {
+        name = "Josh";
+        phone_number = "${config.sops.placeholder.phone_number}";
+      };
+
+      keybindings = {
+        normal = {
+          "ctrl-n" = "select_channel next";
+          "ctrl-p" = "select_channel previous";
+          "ctrl-j" = "select_message next entry";
+          "ctrl-k" = "select_message previous entry";
+        };
+      };
     };
   };
 }
