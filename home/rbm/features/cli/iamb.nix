02871404ee7b7f34ac0d430fea3a3837c9811c7d
redbeardymcgee <redbeardymcgee@proton.me>
2025-11-06 21:16:26 -0600

    add iamb matrix client

--- home/rbm/features/cli/iamb.nix
+++ home/rbm/features/cli/iamb.nix
@@ -1,21 +1,13 @@
 {
-  inputs,
-  pkgs,
-  ...
-}: {
-  home.packages = [
-    inputs.iamb.packages.${pkgs.system}.default
-  ];
-
-  xdg.configFile."iamb/config.toml" = {
+  programs.iamb = {
     enable = true;
 
-    source = (pkgs.formats.toml {}).generate "config.toml" {
-      profiles.user = {
+    settings = {
+      profiles.rbm = {
         user_id = "@redbeardy_mcgee:matrix.org";
-        layout = {
-          style = "restore";
-        };
+      };
+      layout = {
+        style = "restore";
       };
       settings = {
         image_preview = {
