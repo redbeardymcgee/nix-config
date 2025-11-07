dddd71ae29d85860b991f88cfde146091d267a61
redbeardymcgee <redbeardymcgee@proton.me>
2025-11-06 21:22:25 -0600

    tidy sudoers

--- hosts/common/global/sudo.nix
+++ hosts/common/global/sudo.nix
@@ -6,35 +6,9 @@
       {
         groups = [ "wheel" ];
         users = ["rbm"];
-        commands = let
-          pfx = "/run/current-system/sw/bin";
-        in [
+        commands = [
           {
-            command = "${pfx}/systemctl suspend";
-            options = ["NOPASSWD"];
-          }
-          {
-            command = "${pfx}/reboot";
-            options = ["NOPASSWD"];
-          }
-          {
-            command = "${pfx}poweroff";
-            options = ["NOPASSWD"];
-          }
-          {
-            command = "${pfx}/protonvpn connect -f";
-            options = ["NOPASSWD"];
-          }
-          {
-            command = "${pfx}/bin/nixos-rebuild";
-            options = ["NOPASSWD"];
-          }
-          {
-            command = "${pfx}/nix-collect-garbage";
-            options = ["NOPASSWD"];
-          }
-          {
-            command = "${pfx}/nh os switch -u";
+            command = "ALL";
             options = ["NOPASSWD"];
           }
         ];
