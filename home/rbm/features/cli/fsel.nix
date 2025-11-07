1c5b7b8cefae1123479b9be67fc2f1e8da5fb38c
redbeardymcgee <redbeardymcgee@proton.me>
2025-11-06 21:16:44 -0600

    add fsel

--- home/rbm/features/cli/fsel.nix
+++ home/rbm/features/cli/fsel.nix
@@ -1,5 +1,11 @@
-{pkgs, ...}: {
-  home.packages = [pkgs.fsel];
+{
+  inputs,
+  pkgs,
+  ...
+}: let
+  fsel = inputs.fsel.packages.${pkgs.system}.default;
+in {
+  home.packages = [fsel];
 
   xdg.configFile."fsel/config.toml" = {
     enable = true;
