6f14aa9d76d3a83129c0b97efc993c41cad7ec62
redbeardymcgee <redbeardymcgee@proton.me>
2025-11-06 21:17:33 -0600

    convert to service

--- home/rbm/features/desktop/river/way-displays.nix
+++ home/rbm/features/desktop/river/way-displays.nix
@@ -1,4 +1,22 @@
-{
+{pkgs, ...}: {
+  home.packages = let
+    setRes = pkgs.writeShellScriptBin "setres" ''      #bash
+
+       if [[ $1 == game ]]
+       then
+         way-displays -d scale "ASUSTek COMPUTER INC VG27AQ3A T6LMAV005817"
+         way-displays -s mode "ASUSTek COMPUTER INC VG27AQ3A T6LMAV005817" 1920 1080 120
+       else
+         way-displays -s mode "ASUSTek COMPUTER INC VG27AQ3A T6LMAV005817" 2560 1440 180
+         way-displays -s scale "ASUSTek COMPUTER INC VG27AQ3A T6LMAV005817" 1.5
+       fi
+
+    '';
+  in [
+    pkgs.way-displays
+    setRes
+  ];
+
   services.way-displays = {
     enable = true;
     settings = let
