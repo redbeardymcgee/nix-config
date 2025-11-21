88fe85d8a6c48e8bb3fd39a486b1059f82112054
redbeardymcgee <redbeardymcgee@proton.me>
2025-11-20 20:56:12 -0600

    add yazi plugins as inputs

--- flake.nix
+++ flake.nix
@@ -20,6 +20,7 @@
     fsel.url = "github:Mjoyufull/fsel";
     nix-index-database.url = "github:nix-community/nix-index-database";
     nixcats.url = "git+file:///home/rbm/src/redbeardymcgee/nixcats";
+    # nixcats.url = "github:redbeardymcgee/nixcats";
     otter-launcher.url = "github:kuokuo123/otter-launcher";
     posting.url = "github:justdeeevin/posting/flake";
     sops-nix = {
@@ -27,7 +28,48 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     stylix.url = "github:danth/stylix/release-25.05";
+
     yazi.url = "github:sxyazi/yazi";
+    augment-command-yazi = {
+      url = "github:hankertrix/augment-command.yazi";
+      flake = false;
+    };
+    bunny-yazi = {
+      url = "github:stelcodes/bunny.yazi";
+      flake = false;
+    };
+    cd-git-root-yazi = {
+      url = "github:ciarandg/cd-git-root.yazi";
+      flake = false;
+    };
+    duckdb-yazi = {
+      url = "github:wylie102/duckdb.yazi";
+      flake = false;
+    };
+    mediainfo-yazi = {
+      url = "github:boydaihungst/mediainfo.yazi";
+      flake = false;
+    };
+    # official-plugins-yazi = {
+    #   url = "github:yazi-rs/plugins"
+    #   flake = false;
+    # };
+    ouch-yazi = {
+      url = "github:ndtoan96/ouch.yazi";
+      flake = false;
+    };
+    restore-yazi = {
+      url = "github:boydaihungst/restore.yazi";
+      flake = false;
+    };
+    recycle-bin-yazi = {
+      url = "github:uhs-robert/recycle-bin.yazi";
+      flake = false;
+    };
+    wl-clipboard-yazi = {
+      url = "github:grappas/wl-clipboard.yazi";
+      flake = false;
+    };
   };
 
   outputs = {
