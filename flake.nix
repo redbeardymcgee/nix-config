18438a3378e8bc1479e9eb98a21401e00723feff
redbeardymcgee <redbeardymcgee@proton.me>
2025-11-26 12:34:05 -0600

    move to 25.11

--- flake.nix
+++ flake.nix
@@ -12,10 +12,10 @@
   };
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
     systems.url = "github:nix-systems/default-linux";
     nixos-hardware.url = "github:NixOS/nixos-hardware";
-    home-manager.url = "github:nix-community/home-manager/release-25.05";
+    home-manager.url = "github:nix-community/home-manager/release-25.11";
 
     fsel.url = "github:Mjoyufull/fsel";
     nix-index-database.url = "github:nix-community/nix-index-database";
@@ -27,7 +27,7 @@
       url = "github:Mic92/sops-nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    stylix.url = "github:danth/stylix/release-25.05";
+    stylix.url = "github:danth/stylix/release-25.11";
 
     yazi.url = "github:sxyazi/yazi";
     augment-command-yazi = {
