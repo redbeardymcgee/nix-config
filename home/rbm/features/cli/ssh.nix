9fd3b14f94705573dcd94c08b67f46036c001874
redbeardymcgee <redbeardymcgee@proton.me>
2025-11-26 16:09:51 -0600

    update ssh config to 25.11

--- home/rbm/features/cli/ssh.nix
+++ home/rbm/features/cli/ssh.nix
@@ -1,47 +1,47 @@
-{
+{config, ...}: {
   services.ssh-agent.enable = true;
 
   programs.ssh = {
     enable = true;
-    forwardAgent = true;
-    addKeysToAgent = "yes";
-    compression = true;
-    # controlMaster = "yes";
-    controlPersist = "yes";
-    controlPath = "\${XDG_RUNTIME_DIR}/%u@%L-to-%r@%h";
+    enableDefaultConfig = false;
 
-    extraConfig = ''
-      Host github.com
-        User git
-        IdentityFile ~/.ssh/toliman-to-github
-
-      Host gitlab.com
-        User git
-        IdentityFile ~/.ssh/toliman-to-gitlab
-
-      Host mcgeedia
-        Hostname media.mcgee.network
-        IdentityFile ~/.ssh/toliman-to-mcgeedia
-
-      Host perseus
-        Hostname 203.0.113.250
-        port 2222
-        IdentityFile ~/.ssh/toliman-to-perseus
-
-      Host mythicdnor
-        Hostname mythicdnor.ddns.net
-        Port 61222
-        User foundry
-        IdentityFile ~/.ssh/toliman-to-mythicdnor
-
-      Host forgejo
-        Hostname git.mcgee.red
-        User git
-        IdentityFile ~/.ssh/toliman-to-forgejo
-
-      Host git.mcgee.red
-        User git
-        IdentityFile ~/.ssh/toliman-to-forgejo
-    '';
+    matchBlocks = {
+      "*" = {
+        forwardAgent = true;
+        addKeysToAgent = "yes";
+        compression = true;
+        controlPersist = "yes";
+        # controlPath = "$${XDG_RUNTIME_DIR}/%u@%L-to-%r@%h";
+        controlPath = "${config.xdg.stateHome}/%u@%L-to-%r@%h";
+      };
+      "github.com" = {
+        host = "github.com";
+        user = "git";
+        identityFile = "~/.ssh/toliman-to-github";
+      };
+      "gitlab.com" = {
+        host = "gitlab.com";
+        user = "git";
+        identityFile = "~/.ssh/toliman-to-gitlab";
+      };
+      "mcgeedia" = {
+        hostname = "mcgee.network";
+        identityFile = "~/.ssh/toliman-to-mcgeedia";
+      };
+      "perseus" = {
+        hostname = "203.0.113.250";
+        port = 2222;
+        identityFile = "~/.ssh/toliman-to-perseus";
+      };
+      "forgejo" = {
+        hostname = "git.mcgee.red ";
+        user = "git";
+        identityFile = "~/.ssh/toliman-to-forgejo";
+      };
+      "git.mcgee.red" = {
+        user = "git";
+        identityFile = "~/.ssh/toliman-to-forgejo";
+      };
+    };
   };
 }
