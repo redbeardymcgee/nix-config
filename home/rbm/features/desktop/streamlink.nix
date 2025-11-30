887f04588ef5b55c8431cc25f9f421fded852026
redbeardymcgee <redbeardymcgee@proton.me>
2025-11-30 13:22:26 -0600

    add streamlink for twitch

--- home/rbm/features/desktop/streamlink.nix
+++ home/rbm/features/desktop/streamlink.nix
@@ -2,20 +2,24 @@
   home.packages = with pkgs; [
     mpv
     chatterino7
+    chromium # NOTE: GUI depends on this
+    streamlink-twitch-gui-bin
   ];
   programs.streamlink = {
     enable = true;
 
-    settings = {
-      player = "mpv";
-      player-args = "--cache 2048";
-      player-no-close = true;
-    };
+    # settings = {
+    ## WARN: GUI wants to control the full config
+    #   player = "mpv";
+    #   player-args = "--cache 2048";
+    #   player-no-close = true;
+    # };
 
-    plugins = {
-      twitch.settings = {
-        supported-codecs = "h264,h265,av1";
-      };
-    };
+    # plugins = {
+    #   twitch.settings = {
+    ## TODO: Supported in 8.0.0
+    #     twitch-supported-codecs = "h264,h265,av1";
+    #   };
+    # };
   };
 }
