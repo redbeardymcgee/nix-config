2fc45db53a03142ef820a286f811bedbaebe3691
redbeardymcgee <redbeardymcgee@proton.me>
2025-12-01 11:42:18 -0600

    improve aerc config

--- home/rbm/features/cli/aerc.nix
+++ home/rbm/features/cli/aerc.nix
@@ -1,7 +1,49 @@
-{config, ...}: {
+{
+  config,
+  pkgs,
+  ...
+}: {
+  home.packages = [pkgs.w3m];
   programs.aerc = {
     enable = true;
     extraConfig.general.unsafe-accounts-conf = true;
+
+    extraConfig = {
+      filters = {
+        "text/plain" = "colorize";
+        "text/html" = "! w3m -I UTF-8 text/html";
+        "text/calendar" = "calendar";
+        "text/*" = ''bat -fP --file-name="''${AERC_FILENAME}" --style=plain'';
+        ".headers" = "colorize";
+        "message/delivery-status" = "colorize";
+      };
+    };
+
+    templates = {
+      quoted_reply =
+        # gotmpl
+        ''
+
+          {{ if or
+            (eq .OriginalMIMEType "text/html")
+            (contains (toLower .OriginalText) "<html")
+          }}
+          {{- $text := exec `/usr/lib/aerc/filters/html` .OriginalText | replace `\r` `` -}}
+          {{- range split "\n" $text -}}
+            {{- if eq . "References:" }}{{break}}{{end}}
+            {{- if or
+              (eq (len .) 0)
+              (match `^\[.+\]\s*$` .)
+            }}{{continue}}{{end}}
+            {{- printf "%s\n" . | replace `^[\s]+` "" | quote}}
+          {{- end -}}
+          {{- else }}
+          {{- trimSignature .OriginalText | quote -}}
+          {{- end -}}
+
+          {{.Signature -}}
+        '';
+    };
   };
   # accounts.email.accounts.gmail = let
   #   ## FIXME: This is impure, therefore unallowed. The solution is apparently
