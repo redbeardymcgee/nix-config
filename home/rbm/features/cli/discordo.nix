8c42fec9c7f4db004e80cec6c760c654c5caddbc
redbeardymcgee <redbeardymcgee@proton.me>
2025-11-30 22:00:37 -0600

    temporarily disable discordo flake
    Issue submitted upstream to update flake.lock, which should resolve
    build issue

--- home/rbm/features/cli/discordo.nix
+++ home/rbm/features/cli/discordo.nix
@@ -1,8 +1,10 @@
-{
-  programs.discordo = {
+{pkgs, ...}: {
+  ## TODO: Wait for upstream flake to be fixed
+  home.packages = [pkgs.discordo];
+  xdg.configFile."discordo/config.toml" = {
     enable = true;
 
-    settings = {
+    source = (pkgs.formats.toml {}).generate "discordo/config.toml" {
       notifications = {
         sound = {
           enabled = false;
@@ -15,4 +17,20 @@
       };
     };
   };
+  # programs.discordo = {
+  #   enable = true;
+  #
+  #   settings = {
+  #     notifications = {
+  #       sound = {
+  #         enabled = false;
+  #       };
+  #     };
+  #     theme = {
+  #       guilds_tree = {
+  #         auto_expand_folders = false;
+  #       };
+  #     };
+  #   };
+  # };
 }
