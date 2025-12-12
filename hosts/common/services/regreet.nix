7188bfff19599d6d7324d37ec8a06bdc2347fad0
redbeardymcgee <redbeardymcgee@proton.me>
2025-12-12 16:40:59 -0600

    improve greeter screen config

--- hosts/common/services/regreet.nix
+++ hosts/common/services/regreet.nix
@@ -2,6 +2,34 @@
   programs.regreet = {
     enable = true;
 
-    cageArgs = [ "-s" "-m" "last" ];
+    cageArgs = ["-s" "-m" "last"];
+
+    settings = {
+      # env = {
+      #   ENV_VARIABLE = "value";
+      # };
+
+      GTK = {
+        application_prefer_dark_theme = true;
+        cursor_blink = true;
+      };
+
+      commands = {
+        reboot = ["systemctl" "reboot"];
+        poweroff = ["systemctl" "poweroff"];
+      };
+
+      appearance = {
+        greeting_msg = "Welcome back!";
+      };
+
+      widget.clock = {
+        # strftime format argument
+        # See https://docs.rs/jiff/0.1.14/jiff/fmt/strtime/index.html#conversion-specifications
+        format = "%a %H:%M";
+        resolution = "500ms";
+        label_width = 150;
+      };
+    };
   };
 }
