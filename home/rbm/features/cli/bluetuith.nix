38c2edc723c277f5a2aa15a9543dac0eeeba9f11
redbeardymcgee <redbeardymcgee@proton.me>
2026-04-06 18:13:58 -0500

    vim bindings for bluetuith
    These were bugged for a while so didn't push commit, but seems fixed now

--- home/rbm/features/cli/bluetuith.nix
+++ home/rbm/features/cli/bluetuith.nix
@@ -1,5 +1,22 @@
 {
   programs.bluetuith = {
     enable = true;
+
+    settings = {
+      keybindings = {
+        Quit = "q";
+
+        NavigateLeft = "h";
+        NavigateUp = "k";
+        NavigateDown = "j";
+        NavigateRight = "l";
+        NavigateTop = "G";
+        NavigateBottom = "g";
+
+        FilebrowserDirForward = "l";
+        FilebrowserDirBackward = "h";
+        FilebrowserToggleHidden = ".";
+      };
+    };
   };
 }
