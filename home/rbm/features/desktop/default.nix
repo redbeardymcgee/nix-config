4d0ca0f0b8c7aa84eaeeb75d22386bbf90b43314
redbeardymcgee <redbeardymcgee@proton.me>
2026-06-07 17:26:37 -0500

    update package list for 26.05
    bitwarden desktop depends on EOL electron, and i'm trying to migrate
    away from it anyway

--- home/rbm/features/desktop/default.nix
+++ home/rbm/features/desktop/default.nix
@@ -21,30 +21,27 @@
   qt.enable = true;
 
   home = {
-    packages = with pkgs;
-      [
-        bitwarden-desktop
-        gimp
-        grayjay
-        jellyfin-desktop
-        inkscape-with-extensions
-        # ladybird
-        moonlight-qt
-        mpv
-        nextcloud-talk-desktop
-        # pixieditor
-        pw-viz # TODO: crashy
-        qbittorrent
-        qpwgraph
-        signal-desktop
-        streamcontroller
-        # supersonic-wayland
-        supersonic
-        vlc
-      ]
-      ++ (with pkgs-unstable; [
-        pixieditor
-        # grayjay
-      ]);
+    packages = with pkgs; [
+      affine
+      # bitwarden-desktop
+      gimp
+      grayjay
+      jellyfin-desktop
+      inkscape-with-extensions
+      # ladybird
+      moonlight-qt
+      mpv
+      nextcloud-talk-desktop
+      notesnook
+      pixieditor
+      pw-viz # TODO: crashy
+      qbittorrent
+      qpwgraph
+      signal-desktop
+      streamcontroller
+      supersonic-wayland
+      supersonic
+      vlc
+    ];
   };
 }
