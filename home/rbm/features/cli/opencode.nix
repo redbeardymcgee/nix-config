aa98a57457a687215c757617e62f30caaf670eaa
redbeardymcgee <redbeardymcgee@proton.me>
2026-06-07 17:28:56 -0500

    consider testing brother's model

--- home/rbm/features/cli/opencode.nix
+++ home/rbm/features/cli/opencode.nix
@@ -4,7 +4,7 @@
 
     settings = {
       model = "mcgeedia/qwen";
-      lsp = true;
+      # lsp = true;
       provider = {
         mcgeedia = {
           name = "McGeedia";
@@ -17,13 +17,62 @@
 
           models = {
             qwen = {
-              # name = "qwen3.6-27b-awq";
               name = "qwen";
-              id = "qwen3.6-27b-awq";
+              id = "qwen3.6-27b-autoround";
+              tool_call = true;
             };
           };
         };
       };
     };
   };
+  # {
+  #   "plugin": ["oh-my-openagent@latest"],
+  #   "model": "local-vllm/qwen3.6-27b-autoround",
+  #   "agent": {
+  #     "build": { "model": "local-vllm/qwen3.6-27b-autoround" },
+  #     "explore": { "model": "local-vllm/qwen3.6-27b-autoround" },
+  #     "scout": { "model": "local-vllm/qwen3.6-27b-autoround" },
+  #     "oracle": { "model": "local-vllm/qwen3.6-27b-autoround" },
+  #     "librarian": { "model": "local-vllm/qwen3.6-27b-autoround" },
+  #     "title": { "model": "local-vllm/qwen3.6-27b-autoround" },
+  #     "summary": { "model": "local-vllm/qwen3.6-27b-autoround" },
+  #     "compaction": { "model": "local-vllm/qwen3.6-27b-autoround" },
+  #     "plan": { "model": "local-vllm/qwen3.6-27b-autoround" }
+  #   }
+  # }
+
+  # {
+  #   "$schema": "https://raw.githubusercontent.com/code-yeongyu/oh-my-openagent/dev/assets/oh-my-opencode.schema.json",
+  #   "agents": {
+  #     "hephaestus": {
+  #       "model": "local-vllm/qwen3.6-27b-autoround"
+  #     },
+  #     "oracle": {
+  #       "model": "local-vllm/qwen3.6-27b-autoround"
+  #     },
+  #     "librarian": {
+  #       "model": "local-vllm/qwen3.6-27b-autoround"
+  #     },
+  #     "explore": {
+  #       "model": "local-vllm/qwen3.6-27b-autoround"
+  #     },
+  #     "multimodal-looker": {
+  #       "model": "local-vllm/qwen3.6-27b-autoround"
+  #     },
+  #     "prometheus": {
+  #       "model": "local-vllm/qwen3.6-27b-autoround"
+  #     },
+  #     "metis": {
+  #       "model": "local-vllm/qwen3.6-27b-autoround"
+  #     },
+  #     "momus": {
+  #       "model": "local-vllm/qwen3.6-27b-autoround"
+  #     },
+  #     "atlas": {
+  #       "model": "local-vllm/qwen3.6-27b-autoround"
+  #     },
+  #     "sisy: {
+  #       "model": "local-vllm/qwen3.6-27b-autoround"
+  #     }
 }
