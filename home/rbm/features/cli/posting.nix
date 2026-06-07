51b7677f1f7f5e3d70f4d7078d0b6a32169bd3eb
redbeardymcgee <redbeardymcgee@proton.me>
2026-06-07 17:19:45 -0500

    update posting config

--- home/rbm/features/cli/posting.nix
+++ home/rbm/features/cli/posting.nix
@@ -1,31 +1,13 @@
 {
-  programs.posting = {
-    enable = true;
-    # package = inputs.posting.packages.${pkgs.stdenv.hostPlatform.system}.default;
-    settings = {
-      animation = "full";
-      theme = "oxocarbon";
-    };
-    themes = [
-      {
-        name = "oxocarbon";
-        # base08
-        primary = "#3ddbd9";
-        # base0F
-        secondary = "#82cfff";
-        # base0C
-        accent = "#ff7eb6";
-        # base00
-        background = "#161616";
-        # base02
-        surface = "#393939";
-        # base0A
-        error = "#ee5396";
-        # base0D
-        success = "#42be65";
-        # base0E
-        warning = "#be95ff";
-      }
-    ];
-  };
+  inputs,
+  pkgs,
+  ...
+}: {
+  home.packages = [pkgs.posting];
+  # programs.posting = {
+  #   enable = true;
+  #   settings = {
+  #     animation = "full";
+  #   };
+  # };
 }
