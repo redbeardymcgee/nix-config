d443053b69b45aa9fba72b1b1802361997d76236
redbeardymcgee <redbeardymcgee@proton.me>
2026-06-08 21:43:32 -0500

    remove useless custom package
    this portal has been added to nixpkgs

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -2,18 +2,5 @@
 # You can build them using 'nix build .#example'
 pkgs: {
   # example = pkgs.callPackage ./example { };
-  # xdg-desktop-portal-luminous = pkgs.callPackage ./xdg-desktop-portal-luminous.nix { };
 }
-# {
-#   inputs,
-#   lib,
-#   pkgs,
-#   ...
-# }: (
-#   lib.dryFlakes pkgs inputs [
-#   ]
-#   //
-#   (lib.importRest {inherit pkgs lib inputs;}
-#     ../pkgs)
-# )
 
